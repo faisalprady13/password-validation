856f6816dfd084b669dfedd4c72a5e72c5a39b62
faisalprady13 <faisalprady@gmail.com>
2026-04-30 10:18:06 +0200

    add empty method signatures

---
+++ src/main/java/PasswordValidator.java
@@ -0,0 +1,29 @@
+public final class PasswordValidator {
+    public static boolean hasMinLength(String password, int min) {
+        return false;
+    }
+
+    public static boolean containsDigit(String password) {
+        return false;
+    }
+
+    public static boolean containsUpperAndLower(String password) {
+        return false;
+    }
+
+    public static boolean isCommonPassword(String password) // small internal list
+    {
+        return false;
+    }
+
+    // Bonus:
+    public static boolean containsSpecialChar(String password, String allowed) {
+        return false;
+    }
+
+    // Optional:
+    public static boolean isValid(String password) // uses the checks above
+    {
+        return false;
+    }
+}
